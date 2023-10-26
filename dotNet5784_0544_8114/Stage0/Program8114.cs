d0a671352f00716a475e498e38562488b5f0e395
Eliyahu Masinter <eliyahumasinter@gmail.com>
2023-10-26 15:00:42 +0300

    Initial Code + Welcome functions

---
+++ dotNet5784_0544_8114/Stage0/Program8114.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    static void Main(string[] args)
+    {
+        Welcome0554();
+        Welcome8114();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome0554();
+    private static void Welcome8114()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0} welcome to my first console application", name);
+    }
+}

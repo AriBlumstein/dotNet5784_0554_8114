d3d43278b439c56665dd7a74fe69936a440596ce
AriBlumstein <ayb1998@aol.com>
2023-10-26 15:14:00 +0300

    Added program to namespace stage0 Implementation of Welcome0554()

--- dotNet5784_0544_8114/Stage0/Program8114.cs
+++ dotNet5784_0544_8114/Stage0/Program8114.cs
@@ -1,17 +1,20 @@
-﻿partial class Program
+﻿namespace Stage0
 {
-    static void Main(string[] args)
+    partial class Program
     {
-        Welcome0554();
-        Welcome8114();
-        Console.ReadKey();
-    }
+        static void Main(string[] args)
+        {
+            Welcome0554();
+            Welcome8114();
+            Console.ReadKey();
+        }
 
-    static partial void Welcome0554();
-    private static void Welcome8114()
-    {
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine("{0} welcome to my first console application", name);
+        static partial void Welcome0554();
+        private static void Welcome8114()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} welcome to my first console application", name);
+        }
     }
 }

54f9472ba82332c925fd91189f8e73738ee02f53
AriBlumstein <ayb1998@aol.com>
2024-01-04 20:43:30 +0200

    setup database

---
+++ dotNet5784_0544_8114/DalList/DataSource.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Dal;
+
+/// <summary>
+/// database for the ifferent entities
+/// </summary>
+
+internal static class DataSource
+{
+
+    internal static Random random = new Random();
+    internal static List<DO.Dependency> Dependencies{ get; } = new ();
+    internal static List<DO.Engineer> Engineers { get; } = new ();
+
+    internal static List<DO.Task> Tasks { get; } = new ();
+
+
+    internal class Config
+    {
+        internal static int dependencyID = 1;
+        internal static int NextdepencyID { get => dependencyID++; }
+
+        internal static int taskID = 1;
+        internal static int TaskID { get => dependencyID++; }
+    }
+
+}

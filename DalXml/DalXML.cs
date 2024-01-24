7e13d1e9b2ec7af354ae031f60efa95247d36550
AriBlumstein <ayb1998@aol.com>
2024-01-24 17:17:03 +0200

    dalxml change for standard namespace presentation

--- DalXml/DalXML.cs
+++ DalXml/DalXML.cs
@@ -2,25 +2,26 @@
 
 using DalApi;
 
-namespace DalXml
+namespace DalXml;
+
+using DalApi;
+
+sealed public class DalXML : IDal
 {
-    sealed public class DalXML : IDal
+    public ITask Task => new TaskImplementation();
+
+    public IEngineer Engineer => new EngineerImplementation();
+
+    public IDependency Dependency => new DependencyImplementation();
+
+    public IConfig Config => new ConfigImplementation();
+
+    public void Reset()
     {
-        public ITask Task => new TaskImplementation();
+        Task.Reset();
+        Engineer.Reset();
+        Dependency.Reset();
+        Config.Reset();
 
-        public IEngineer Engineer => new EngineerImplementation();
-
-        public IDependency Dependency => new DependencyImplementation();
-
-        public IConfig Config => new ConfigImplementation();
-
-        public void Reset()
-        {
-            Task.Reset();
-            Engineer.Reset();
-            Dependency.Reset();
-            Config.Reset();
-
-        }
     }
 }

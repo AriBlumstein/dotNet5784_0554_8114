1cc06e143c26b44abb1c12afb66270e5680cafa9
AriBlumstein <ayb1998@aol.com>
2024-02-03 22:35:50 +0200

    master logic interface for BL

---
+++ BL/BlApi/IBl.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace BlApi;
+
+/// <summary>
+/// master interface for the api interfaces
+/// </summary>
+public interface IBl
+{
+    public IEngineer Engineer { get; }
+    public IMilestone Milestone { get; }
+    public ITask Task { get; }
+
+}

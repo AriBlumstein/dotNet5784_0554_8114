5b1c558209dd3721b045ef22308291d43dc16352
AriBlumstein <ayb1998@aol.com>
2024-02-04 00:19:50 +0200

    update BO. engineer to include a valid email field

--- BL/BO/Engineer.cs
+++ BL/BO/Engineer.cs
@@ -8,6 +8,7 @@ public class Engineer
 {
     public int ID { get; init; }
     public String Name { get; set; }
+    public String Email { get; set; }
     public EngineerExperience Level { get; set; }
     public double Cost { get; set; }
     public TaskInEngineer? Task { get; set; }

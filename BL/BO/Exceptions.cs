bce6ec99b6cef780eab8be9a5c634a78f87a71c0
AriBlumstein <ayb1998@aol.com>
2024-02-03 22:50:18 +0200

    Add exceptions to the BL exceptions

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -2,6 +2,17 @@
 
 namespace BO;
 
-internal class Exceptions
+
+[Serializable]
+public class BlDoesNotExistException : Exception
 {
+    public BlDoesNotExistException(string? message) : base(message) { }
+    public BlDoesNotExistException(string? message, Exception innerException) : base(message, innerException) { }
+}
+
+
+[Serializable]
+public class BlNullPropertyException : Exception
+{
+    public BlNullPropertyException(string? message) : base(message) { }
 }

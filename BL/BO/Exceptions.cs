2ce6fe71f4249c4dbfbe5cc2ae3752e50f21b4e7
AriBlumstein <ayb1998@aol.com>
2024-02-04 00:20:13 +0200

    Add various exceptions possible Bl.exceptions

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -16,3 +16,22 @@ public class BlNullPropertyException : Exception
 {
     public BlNullPropertyException(string? message) : base(message) { }
 }
+
+[Serializable]
+
+public class BlIllegalPropertyException : Exception
+{
+    public BlIllegalPropertyException(string? message) : base(message) { }
+}
+
+[Serializable]
+public class BlAlreadyExistsException : Exception
+{
+    public BlAlreadyExistsException(string? message, Exception e) : base(message, e) { }
+}
+
+[Serializable]
+public class BlIllegalOperationException : Exception
+{
+    public BlIllegalOperationException(string? message) : base(message) { }
+}

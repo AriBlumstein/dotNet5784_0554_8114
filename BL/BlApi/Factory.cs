0b0a5c358b9edc29fa1e06900ab74935dffc1cbd
AriBlumstein <ayb1998@aol.com>
2024-02-06 19:02:14 +0200

    factory for Bl

---
+++ BL/BlApi/Factory.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace BlApi;
+
+public class Factory
+{
+    public static IBl Get() => new BlImplementation.Bl();
+}
--- BL/BlApi/IBl.cs
+++ BL/BlApi/IBl.cs
@@ -7,6 +7,8 @@ namespace BlApi;
 /// </summary>
 public interface IBl
 {
+
+
     public IEngineer Engineer { get; }
     public IMilestone Milestone { get; }
     public ITask Task { get; }

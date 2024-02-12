4cc9ae3c91f43dac578a127ffdeb1139742f1315
AriBlumstein <ayb1998@aol.com>
2024-02-12 21:31:52 +0200

    main window Handle Task and Init data buttons

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -9,16 +9,38 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PL
+namespace PL;
+
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
+
+    private void showTaskList_Click(object sender, RoutedEventArgs e)
+    {
+        new TaskListWindow().Show();
+    }
+
+    private void initializeData_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBoxResult result = MessageBox.Show("Do you want to proceed? (doing so will reset data if there was)", "Question", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+        if (result == MessageBoxResult.Yes)
+        {
+            DalTest.Initialization.Do();
+        }
+        
+    }
+
 }

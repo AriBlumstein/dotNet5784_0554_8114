260ab33ce5fc02cafae0ed280fc526ebc1141ec2
AriBlumstein <ayb1998@aol.com>
2024-02-14 23:46:46 +0200

    handle closing event in EngineerListWindow from EngineerWindow

--- PL/Engineer/EngineerListWindow.xaml.cs
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace PL.Engineer
         public EngineerListWindow()
         {
             InitializeComponent();
-            EngineerList = s_bl?.Engineer.ReadAll()!;
+            
         }
 
         public IEnumerable<BO.Engineer> EngineerList
@@ -46,18 +46,30 @@ namespace PL.Engineer
 
         }
 
+        private void engineerWindow_Closed(Object Sender, EventArgs e)
+        {
+            EngineerList = (Experience == BO.EngineerExperience.None) ?
+            s_bl?.Engineer.ReadAll()! : s_bl?.Engineer.ReadAll(item => (BO.EngineerExperience?)item.Exp == Experience)!;
+        }
+
         private void btnAddNewEngineer(object sender, RoutedEventArgs e)
         {
-            new EngineerWindow().ShowDialog();
-            cbExperience_SelectionChanged(this, null);
+            EngineerWindow newWindow = new EngineerWindow();
+            newWindow.Closed += engineerWindow_Closed!;
+            newWindow.ShowDialog();
+            
         }
 
         private void listClickUpdateEngineer(object sender, RoutedEventArgs e)
         {
             BO.Engineer? engineer = (sender as ListView)?.SelectedItem as BO.Engineer;
 
-            new EngineerWindow(engineer.ID).ShowDialog();
-            cbExperience_SelectionChanged(this, null);
+            if (engineer!=null)
+            {
+                EngineerWindow newWindow = new EngineerWindow(engineer.ID);
+                newWindow.Closed += engineerWindow_Closed!;
+                newWindow.ShowDialog();
+            }
         }
 
 

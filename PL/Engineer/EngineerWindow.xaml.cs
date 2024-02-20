d60e021c08bf9ee4b6d7ac5c80e8453920b587c8
Eliyahu Masinter <eliyahumasinter@gmail.com>
2024-02-20 16:54:25 +0200

    Engineer window remove task

--- PL/Engineer/EngineerWindow.xaml.cs
+++ PL/Engineer/EngineerWindow.xaml.cs
@@ -11,13 +11,13 @@ namespace PL.Engineer
     /// </summary>
     public partial class EngineerWindow : Window
     {
-        
+
         private readonly IBl s_bl = Factory.Get();
 
 
         public static readonly DependencyProperty EngineerProperty =
          DependencyProperty.Register("Engineer", typeof(BO.Engineer), typeof(EngineerWindow), new PropertyMetadata(null));
-        
+
         public BO.Engineer Engineer
         {
             get { return (BO.Engineer)GetValue(EngineerProperty); }
@@ -37,42 +37,51 @@ namespace PL.Engineer
         }
 
 
-        public static readonly DependencyProperty PossibleTasksProperty=
-            DependencyProperty.Register("PossibleTasks", typeof(ObservableCollection<int>), typeof(EngineerWindow), new PropertyMetadata(null));
+        public static readonly DependencyProperty PossibleTasksProperty =
+            DependencyProperty.Register("PossibleTasks", typeof(ObservableCollection<int?>), typeof(EngineerWindow), new PropertyMetadata(null));
 
-        public ObservableCollection<int> PossibleTasks
+        public ObservableCollection<int?> PossibleTasks
         {
-            get { return (ObservableCollection<int>)GetValue(PossibleTasksProperty); }
+            get { return (ObservableCollection<int?>)GetValue(PossibleTasksProperty); }
             set { SetValue(PossibleTasksProperty, value); }
         }
 
 
-        public EngineerWindow(int id=0)
+        public EngineerWindow(int id = 0)
         {
             InitializeComponent();
-            if(id == 0)
+            if (id == 0)
             {
                 Engineer = new BO.Engineer();
-                
+
             }
             else
             {
                 Engineer = s_bl?.Engineer.Read(id)!;
-                
-                
+
+
 
             }
 
-            PossibleTasks = new ObservableCollection<int>(s_bl.Task.ReadAll(t => (EngineerExperience?)t.Difficulty <= Engineer.Level).Select(t => t.ID));
-
+            PossibleTasks = new ObservableCollection<int?>(s_bl.Task.ReadAll(t => (EngineerExperience?)t.Difficulty <= Engineer.Level).Select(t => t.ID).Select(i=>(int?)i));
+            PossibleTasks.Add(null);
+            if (Engineer.Task != null)
+            {
+                TaskID = Engineer.Task.ID;
+            }
 
         }
 
         private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
         {
-           
+
+            if (TaskID != null)
+            {
+                Engineer.Task = new TaskInEngineer { ID = TaskID.Value };
+            }
+
             Button button = sender as Button;
-            if(button.Content.ToString()=="Update")
+            if (button.Content.ToString() == "Update")
             {
 
 
@@ -90,25 +99,25 @@ namespace PL.Engineer
                 {
                     MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
-                catch(BlIllegalPropertyException ex)
+                catch (BlIllegalPropertyException ex)
                 {
                     MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
-                catch(BlIllegalOperationException ex)
+                catch (BlIllegalOperationException ex)
                 {
                     MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
-                
+
             }
-            else if(button.Content.ToString()=="Add")
-            
+            else if (button.Content.ToString() == "Add")
+
                 try
                 {
-                    
+
                     Engineer = s_bl?.Engineer.Create(Engineer)!;
                     MessageBox.Show($"Successfully added engineer {Engineer.ID}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
                     Close();
@@ -137,19 +146,22 @@ namespace PL.Engineer
 
         }
 
+
         private void Experience_Level_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (Engineer.Level==BO.EngineerExperience.None)
+            if (Engineer.Level == BO.EngineerExperience.None)
             {
-                PossibleTasks=new ObservableCollection<int> { };
+                PossibleTasks = new ObservableCollection<int?> { null };
             }
             else
             {
 
-            PossibleTasks = new ObservableCollection<int>(s_bl.Task.ReadAll(t => (EngineerExperience?)t.Difficulty <= Engineer.Level).Select(t => t.ID));
+                PossibleTasks = new ObservableCollection<int?>(s_bl.Task.ReadAll(t => (EngineerExperience?)t.Difficulty <= Engineer.Level).Select(t => t.ID).Select(i => (int?)i));
+                PossibleTasks.Add(null);
             }
 
         }
-    }
+
     }
 
+}

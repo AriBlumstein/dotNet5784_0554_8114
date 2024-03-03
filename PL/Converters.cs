42cd76135e6c68c79c559d23133207c325a37164
Eliyahu Masinter <eliyahumasinter@gmail.com>
2024-03-03 23:27:04 +0200

    BoolToinvisibility converter

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -193,9 +193,22 @@ namespace PL
         {
             throw new NotImplementedException();
         }
+
+
     }
 
+    class BoolToInvisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value == true ? Visibility.Collapsed : Visibility.Visible;
+        }
 
-   
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 
 }
